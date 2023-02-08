adc8a0ea0f909588696ace29a377ca7123b8df49
AsiyaAkther <mahicse28@gmail.com>
2023-02-08 19:32:51 +0600

    Day 8 - skill section design done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-//============== Counter 
+//============== Counter
 
 $('.counter').counterUp({
     time : 3000
+});
+
+//============ Isotope
+$('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
 });
